225a2828b6f5d64e4a22f58651fc9206518c3157
mbalicc <meho.balic.23@size.ba>
2025-08-30 20:34:16 +0200

    prvi commit

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+// Test ruta
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello World' });
+});
+
+// Pokretanje servera
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
